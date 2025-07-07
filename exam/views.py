cb163a4ee3a00ec23941d5c1f550c40aa2e5a528
mehta-g1 <vikash.kumarmehta.bca.2023@mitmeerut.ac.in>
2025-07-07 10:49:04 +0530

    first commit

---
+++ exam/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+class Questions:
+    def __init__(self,que,a,b,c,d):
+        self.que = que
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+# def about(requ)
+
+def contact(request) :
+    return render(request,'contact.html')
+
+def Home(request) :
+    return render(request,'home.html')
+
+def About(request) :
+    return render(request,'about.html')
+
+def questions(request) :
+    questions = [
+        Questions("1. Which of the following is NOT a core programming concept?","Variables","Conditionals","Algorithms","Functions"),
+        Questions("2. What does the acronym 'OOP' stand for in programming?","Object-Oriented Programming","Operational Output Programming","Organized Online Procedures","Output Object Programming "),
+        Questions("3. Which language is commonly used for both front-end and back-end web development?","Java","C#","JavaScript","Python "),
+        Questions("4. Which type of language is used to automate tasks and manage dynamic web content?","Compiled Language","Machine Language","Scripting Language","Low-level Language "),
+        Questions("5. What is the primary function of a compiler in programming?","To execute code directly","To translate source code into machine code","To debug code","To store data ")
+    ]
+     
+    data = {'questions':questions}
+
+    return render(request,'testpaper.html',data)
