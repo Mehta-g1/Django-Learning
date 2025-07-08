61d47dcb73b8cf0964918ddf2635aa146935fc47
mehta-g1 <vikash.kumarmehta.bca.2023@mitmeerut.ac.in>
2025-07-08 12:21:54 +0530

    tt

--- exam/models.py
+++ exam/models.py
@@ -4,3 +4,8 @@ class Employee(models.Model):
     eno = models.IntegerField()
     ename = models.CharField(max_length=100)
     esal = models.FloatField()
+
+
+    def __str__(self):
+        return self.ename
+
